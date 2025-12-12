8796b8763b23639a29a485b0f060884d1c5b1058
Tabari Roper <tabariroper14@icloud.com>
2025-12-11 20:53:11 -0500

    FIX: CORS headers for feedback submission

--- api/portal_submit_feedback.js
+++ api/portal_submit_feedback.js
@@ -28,6 +28,12 @@ export default async function handler(req, res) {
   const origin = req.headers.origin || '*';
   res.setHeader('Access-Control-Allow-Origin', origin);
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   
